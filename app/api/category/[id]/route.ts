253239b3e100d99cee74dfc88abbd4ed49d7e422
shiinedev <shiinecadde17@gmail.com>
2025-09-05 19:19:47 +0100

    update category delete

--- app/api/category/[id]/route.ts
+++ app/api/category/[id]/route.ts
@@ -1,22 +1,30 @@
-"use server"
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function DELETE(_req:Request, { params }: { params: { id: string }}) {
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
 
-    const {id} = params;
-    
-    try {
-        if(!id) return NextResponse.json({error:"id not found"},{status:404,statusText:"id not found"}) ;
+  if (!id) {
+    return NextResponse.json({ error: "id not found" }, { status: 404 });
+  }
 
-         await prisma.category.delete({
-            where:{id}
-        })
+  try {
+    const category = await prisma.category.findUnique({ where: { id } });
 
-        return NextResponse.json({message:"delete category successfully"},{status:200})
-
-    } catch (error) {
-        return NextResponse.json({error:error},{status:401});
+    if (!category) {
+      return NextResponse.json({ error: "category not found" }, { status: 404 });
     }
 
+    await prisma.category.delete({ where: { id } });
+
+    return NextResponse.json(
+      { message: "category deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: String(error) }, { status: 500 });
+  }
 }
