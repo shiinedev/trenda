c7329fc286ef798ba30ae202f691fa4efbda3899
shiinedev <shiinecadde17@gmail.com>
2025-09-06 06:13:05 +0100

    added delete category with type params

---
+++ app/api/category/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+
+type Params = { params: { id: string } };
+
+export async function DELETE(_req:NextRequest,{params}:Params){
+
+    const {id} =  params;
+
+    if(!id) return NextResponse.json({error:"id is not defined"},{status:404})
+        try {
+            const category = await prisma.category.findUnique({
+                where:{id}
+            });
+
+            if(!category) return NextResponse.json({error:"category is not defined"},{status:404});
+
+            await prisma.category.delete({
+                where:{id}
+            });
+
+            return NextResponse.json({message:"category deleted successfully"},{status:200})
+
+        } catch (error) {
+            return NextResponse.json({error: error},{status:500})
+        }
+}
